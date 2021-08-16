7ab31c2193c7afd23d03b4b94363643e20ffd9e9
Palacharla Narendra <palacharlanarendra@gmail.com>
2021-08-16 12:12:53 +0530

    user auth completed

---
+++ block-BNaafu/DEMO/models/expenseModel.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var expenseSchema = new Schema(
+  {
+    category: { type: String },
+    expenseAmount: { type: Number },
+    expenseDate: { type: String },
+    // userId : {type : Schema.Types.ObjectId, required : true, ref : "User"}
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Expense', expenseSchema);
